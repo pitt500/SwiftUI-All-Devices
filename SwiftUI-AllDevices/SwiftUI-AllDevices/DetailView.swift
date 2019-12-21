9839a059fa036ca25e5a71edbb0593e95ce23f90
pitt500 <pedro.rojaslo50@gmail.com>
2019-12-21 15:09:18 -0700

    Implements views for iOS

---
+++ SwiftUI-AllDevices/SwiftUI-AllDevices/DetailView.swift
@@ -0,0 +1,29 @@
+//
+//  SwiftUIView.swift
+//  SwiftUI-AllDevices
+//
+//  Created by projas on 12/21/19.
+//  Copyright © 2019 projas. All rights reserved.
+//
+
+import SwiftUI
+
+struct DetailView: View {
+  
+  let animal: Animal
+  
+  var body: some View {
+    VStack {
+      Text(animal.image)
+        .font(.custom("Arial", size: 100))
+      Text(animal.name)
+      Text(animal.description)
+    }
+  }
+}
+
+struct SwiftUIView_Previews: PreviewProvider {
+  static var previews: some View {
+    DetailView(animal: Animal(name: "Cat", description: "This is a cat", image: "🐈") )
+  }
+}
