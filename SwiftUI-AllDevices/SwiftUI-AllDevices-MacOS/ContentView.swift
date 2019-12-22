06cd0abaf29a96c064381d50f7547c2867bcd0c7
pitt500 <pedro.rojaslo50@gmail.com>
2019-12-21 21:11:03 -0700

    Implement MacOS View

--- SwiftUI-AllDevices/SwiftUI-AllDevices-MacOS/ContentView.swift
+++ SwiftUI-AllDevices/SwiftUI-AllDevices-MacOS/ContentView.swift
@@ -8,16 +8,58 @@
 
 import SwiftUI
 
-struct ContentView: View {
-    var body: some View {
-        Text("Hello, World!")
-            .frame(maxWidth: .infinity, maxHeight: .infinity)
+struct DetailView: View {
+  let animal: Animal
+  
+  var body: some View {
+    VStack {
+      Text(animal.image)
+        .font(.custom("Arial", size: 200))
+      Text(animal.name)
+      Text(animal.description)
     }
+  }
+}
+
+struct AnimalList: View {
+  private let animals = AnimalService.getAll()
+  @Binding var selectedAnimal: Animal?
+  
+  var body: some View {
+    List(self.animals, id: \.name) { animal in
+      VStack {
+        Text(animal.image)
+          .font(.custom("Arial", size: 100))
+        Text(animal.name)
+      }.onTapGesture {
+        self.selectedAnimal = animal
+      }
+    }
+  }
+}
+
+struct ContentView: View {
+  
+  @State private var selectedAnimal: Animal? = nil
+  
+  var body: some View {
+    NavigationView {
+      
+      HStack {
+        AnimalList(selectedAnimal: self.$selectedAnimal)
+        DetailView(animal: self.selectedAnimal ?? Animal.placeholder)
+        .frame(maxWidth: 200)
+      }
+      
+      
+    }
+    .navigationViewStyle(DoubleColumnNavigationViewStyle())
+  }
 }
 
 
 struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView()
-    }
+  static var previews: some View {
+    ContentView()
+  }
 }
